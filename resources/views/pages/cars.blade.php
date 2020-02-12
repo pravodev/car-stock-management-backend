fdb7065f9ceb07eed5bb2affa6a281e30edaf0e6
pravodev <pravodev@gmail.com>
2020-02-12 17:34:19 +0700

    WIP: cars

--- resources/views/pages/cars.blade.php
+++ resources/views/pages/cars.blade.php
@@ -1,7 +1,28 @@
 @extends('template.app')
 @section('title', 'List Mobil')
 @section('content')
-<div class="container">
-    
+<div class="container content">
+    <div class="text-right">
+        <button class="btn btn-primary">Tambah Mobil</button>
+    </div>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>No.</th>
+                <th>Nama</th>
+                <th>Harga</th>
+                <th>Stock</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+    </table>
 </div>
 @endsection
+
+@push('footer')
+<script>
+    jQuery(document).ready(function(){
+        
+    })
+</script>
+@endpush
